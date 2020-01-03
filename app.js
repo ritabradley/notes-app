e09d670930f33009aa2e7d3b9c72bbfa8b195ba5
Rita Bradley <contact@ritabradley.com>
2020-01-03 12:59:56 -0800

    Finished app

--- app.js
+++ app.js
@@ -3,67 +3,59 @@ const yargs = require('yargs');
 const notes = require('./notes');
 
 yargs.command({
-  command: 'add',
-  describe: 'Add a new note',
-  builder: {
-    title: {
-      describe: 'Note title',
-      demandOption: true,
-      type: 'string'
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
     },
-    body: {
-      describe: 'Note body',
-      demandOption: true,
-      type: 'string'
+    handler(argv) {
+        notes.addNote(argv.title, argv.body);
     }
-  },
-  handler(argv) {
-    notes.addNote(argv.title, argv.body);
-  }
 });
 
 yargs.command({
-  command: 'remove',
-  describe: 'Remove a note',
-  builder: {
-    title: {
-      describe: 'Note title',
-      demandOption: true,
-      type: 'string'
-    }
-  },
-  handler(argv) {
-    notes.removeNote(argv.title);
-  }
-});
-yargs.command({
-  command: 'read',
-  describe: 'Read a note',
-  builder: {
-    title: {
-      describe: 'Note title',
-      demandOption: true
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
     },
-    body: {
-      describe: 'Note body',
-      demandOption: true
+    handler(argv) {
+        notes.removeNote(argv.title);
     }
-  },
-  handler() {
-    console.log(chalk.bgRed.bgRgb(1, 181, 226).white.bold('Reading a note'));
-  }
 });
 yargs.command({
-  command: 'list',
-  describe: 'List all notes',
-  handler() {
-    console.log(
-      chalk.bgRed
-        .rgb(255, 204, 0)
-        .bgRgb(255, 128, 0)
-        .bold('Listing all notes')
-    );
-  }
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
+    }
+});
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler(argv) {
+        notes.listNotes(argv);
+    }
 });
 
 yargs.parse();
--- notes.js
+++ notes.js
@@ -2,61 +2,78 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 const getNotes = () => {
-  return chalk.bgBlueBright.white.bold('Your notes...');
+    return chalk.bgBlueBright.white.bold('Your notes...');
 };
 
 // Function to add a note to the notes.json file
 
 const addNote = (title, body) => {
-  // *Loads array of notes
-  const notes = loadNotes();
-  // *Filters out the note with a title that matches the passed title argument
-  const duplicates = notes.filter(note => note.title === title);
-  if (duplicates.length === 0) {
-    notes.push({
-      title,
-      body
-    });
+    // *Loads array of notes
+    const notes = loadNotes();
+    // *Filters out the note with a title that matches the passed title argument
+    const duplicates = notes.filter(note => note.title === title);
+    if (duplicates.length === 0) {
+        notes.push({
+            title,
+            body
+        });
 
-    saveNotes(notes);
-  } else {
-    return 'Choose a different title';
-  }
+        saveNotes(notes);
+    } else {
+        return 'Choose a different title';
+    }
 };
 
 // Function to remove a note from the JSON file by comparing the passed title argument
 
 const removeNote = title => {
-  const notes = loadNotes();
-  // * Filter the notes array and keep the notes that don't match the title passed as an argument
-  const keptNotes = notes.filter(note => note.title !== title);
+    const notes = loadNotes();
+    // * Filter the notes array and keep the notes that don't match the title passed as an argument
+    const keptNotes = notes.filter(note => note.title !== title);
 
-  notes.length > keptNotes.length
-    ? console.log(chalk.bgGreen.white('Note removed!'))
-    : console.log(chalk.bgRed.white('No note removed!'));
+    notes.length > keptNotes.length
+        ? console.log(chalk.bgGreen.white('Note removed!'))
+        : console.log(chalk.bgRed.white('No note removed!'));
 
-  saveNotes(keptNotes);
+    saveNotes(keptNotes);
+};
+
+const listNotes = () => {
+    const notes = loadNotes();
+    console.log(chalk.bgBlueBright.white.bold('Your notes...'));
+
+    const list = notes.forEach(note => {
+        console.log(note.title);
+    });
+};
+
+const readNote = title => {
+    const notes = loadNotes();
+    const noteToRead = notes.filter(note => note.title === title);
+
+    console.log(noteToRead);
 };
 
 const saveNotes = notes => {
-  const data = JSON.stringify(notes);
-  fs.writeFileSync('notes.json', data);
+    const data = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', data);
 };
 
 const loadNotes = () => {
-  try {
-    const buffer = fs.readFileSync('./notes.json');
-    const data = buffer.toString();
-    const parsedData = JSON.parse(data);
+    try {
+        const buffer = fs.readFileSync('./notes.json');
+        const data = buffer.toString();
+        const parsedData = JSON.parse(data);
 
-    return parsedData;
-  } catch (error) {
-    return [];
-  }
+        return parsedData;
+    } catch (error) {
+        return [];
+    }
 };
 
 module.exports = {
-  getNotes,
-  addNote,
-  removeNote
+    addNote,
+    removeNote,
+    readNote,
+    listNotes
 };
